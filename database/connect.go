41f9853c243a741d785ad33dcf01f8d6e1f78624
Elias Jurado <ridimdev@gmail.com>
2024-03-25 17:32:41 -0500

    conexion a la db

---
+++ database/connect.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func Connect() (*sql.DB, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	dns := fmt.Sprintf("%v:%v@(%v:%v)/%v",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"))
+
+	//Abrir conexión a la db
+	db, err := sql.Open("mysql", dns)
+	if err != nil {
+		return nil, err
+	}
+
+	//Verificar conexión a la db
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("%+v\n", "conexion exitosa")
+	return db, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"gomysql/database"
+	"log"
 
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db,err := database.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
 }
